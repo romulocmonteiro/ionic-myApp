36054a8aafb78a2626833f29097545f351b45725
Romulo Monteiro <romulocmonteiro@MacBook-Pro-de-Romulo.local>
2018-05-20 21:41:17 -0300

    enter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,38 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { IntroPageModule } from '../pages/intro/intro.module';
+import { MenuPageModule } from '../pages/menu/menu.module';
+import { FeedPageModule } from '../pages/feed/feed.module';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  declarations: [MyApp, HomePage, TabsPage],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp), IntroPageModule, MenuPageModule, FeedPageModule],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+  entryComponents: [MyApp, HomePage, TabsPage],
+  providers: [StatusBar, SplashScreen, { provide: ErrorHandler, useClass: IonicErrorHandler }]
 })
-export class AppModule {}
+export class AppModule { }
