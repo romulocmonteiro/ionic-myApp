6b09c8fc36b7c7de210740bb4c284d65527497f7
Romulo Monteiro <romulocmonteiro@MacBook-Pro-de-Romulo.local>
2018-06-15 21:56:00 -0300

    Versão 0.3.0 com revisão no layout da tela de detalhes dos filmes

--- src/pages/movie-detail/movie-detail.ts
+++ src/pages/movie-detail/movie-detail.ts
@@ -15,6 +15,7 @@ export class MovieDetailPage {
   public json_similar_movies    = new Array<any>();
   public json_external_ids      = new Array<any>();
   public json_movie_credits     = new Array<any>();
+  public grid                   = new Array<any>();
 
   constructor(
     public navCtrl: NavController, 
@@ -28,10 +29,12 @@ export class MovieDetailPage {
     console.log('Movie_id = '+ movie_id);
     this.navCtrl.push("MovieDetailPage");
   }
-  
+
   ionViewDidLoad() {
 
-    let config = JSON.parse(localStorage.getItem("config"));  
+    let config = JSON.parse(localStorage.getItem("config"));
+
+    let rowNum = 0; //counter to iterate over the rows in the grid
 
     // Aqui foi chamada a funçao criada dentro do provider
     this.movieProvider.getMovieById(config.movie_id).subscribe(
@@ -47,7 +50,6 @@ export class MovieDetailPage {
             this.json_external_ids = JSON.parse(response_external_ids._body);
             console.log('MovieDetailPage - IDs externos carregados');
             console.log(data_external_ids);
-            console.log(this.json_external_ids);
           },
           error_external_ids => { console.log(error_external_ids); }
         )
@@ -57,9 +59,28 @@ export class MovieDetailPage {
             const response_movie_credits = (data_movie_credits as any);
             const obj_return = JSON.parse(response_movie_credits._body);
             this.json_movie_credits = obj_return.cast;
-                console.log('MovieDetailPage - Créditos carregados');
+
+            // formando uma grid 3x3 de no máximo 15 elementos
+            for (let i = 0; i < 15; i+=3) { //iterate images
+              this.grid[rowNum] = Array(3); //declare two elements per row
+          
+              if (this.json_movie_credits[i]) { //check file URI exists
+                this.grid[rowNum][0]= this.json_movie_credits[i]
+              }
+          
+              if (this.json_movie_credits[i+1]) { //repeat for the second image
+                this.grid[rowNum][1] = this.json_movie_credits[i+1]
+              }
+          
+              if (this.json_movie_credits[i+2]) { 
+                this.grid[rowNum][2] = this.json_movie_credits[i+2]
+              }
+
+              rowNum++; //go on to the next row
+            }
+
+            console.log('MovieDetailPage - Créditos carregados');
             console.log(data_movie_credits);
-            console.log(this.json_movie_credits[0].character);
           },
           error_external_ids => { console.log(error_external_ids); }
         )
